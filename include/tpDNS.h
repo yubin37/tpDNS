27864c2260b036770a618adae9923dbbafd077a1
Eric <yubin@cnyubin.com>
2015-03-16 23:32:56 +0800

    add hash

--- include/tpDNS.h
+++ include/tpDNS.h
@@ -1,10 +1,13 @@
 #ifndef _TP_DNS_INCLUDE_H_
 #define _TP_DNS_INCLUDE_H_
 
+#include <stdint.h>
 #include <stdio.h>
 #include <syslog.h>
 
 #define PROGRAM_NAME "TP-DNS"
 #define LOG_OPTIONS  LOG_CONS | LOG_PID
 #define LOG_FACILITY LOG_LOCAL1
+
+typedef  uint64_t  hash_type;
 #endif
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-tpDNS: tpDNS.c include/tpDNS.h
-	gcc -o tpDNS tpDNS.c
+tpDNS: tpDNS.c include/tpDNS.h include/tpHash.h src/tpHash.c
+	gcc -o tpDNS tpDNS.c 
 clean:
 	rm tpDNS
